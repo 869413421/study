d9df6f55967c828bf1155c568074b4ffbc8ff671
869413421 <13528685024@163.com>
2019-11-21 22:09:37 +0800

    client

--- client/TcpClient.php
+++ client/TcpClient.php
@@ -3,10 +3,14 @@
 //创建一个客户端并且指定协议
 $client = new swoole_client(SWOOLE_SOCK_TCP);
 
-$client->connect('127.0.0.1', 9501);
+if(!$client->connect('127.0.0.1', 9501)){
+    echo '连接失败';
+    exit();
+}
 
 //接收CLI模式下用户输入内容
-$msg = fwrite(STDOUT, '请输入：');
+fwrite(STDOUT, '请输入：');
+$msg=trim(fgets(STDIN));
 
 //发送内容到客户端
 $client->send($msg);
