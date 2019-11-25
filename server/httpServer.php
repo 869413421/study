197916928faf83d81e06a1a5c57e5f0ee1fe775b
Your Name <you@example.com>
2019-11-25 10:21:08 +0800

    添加静态目录

--- server/httpServer.php
+++ server/httpServer.php
@@ -3,9 +3,15 @@
 $httpServer = new swoole_http_server('0.0.0.0', 8811);
 
 //接受请求的回调函数
-$httpServer->on('request', function (swoole_http_request $request, swoole_http_response $response) {
+$httpServer->on('request', function (swoole_http_request $request, swoole_http_response $response)
+{
     $get = $request->get;
     $response->end('get数据：' . json_encode($get));
 });
+
+$httpServer->set([
+    'enable_static_handler'=>true,
+    'document_root'=>'/root/study/static'
+]);
 
 $httpServer->start();
