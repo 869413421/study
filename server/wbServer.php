6541e82f5caccf99eab5ae1e6ed1d6159c199592
869413421 <13528685024@163.com>
2019-11-25 23:24:35 +0800

    wbsocket

--- server/wbServer.php
+++ server/wbServer.php
@@ -1,3 +1,42 @@
 <?php
 
-$wbServer = new self();
+class wbServer
+{
+    private $HOST = '0.0.0.0';
+    private $PORT = 8811;
+    private $instance = null;
+
+    public function __construct()
+    {
+        $this->instance = new swoole_websocket_server($this->HOST, $this->PORT);
+
+        $this->instance->set([
+            'document_root' => '/root/study/static/', // v4.4.0以下版本, 此处必须为绝对路径
+            'enable_static_handler' => true,
+        ]);
+
+        $this->instance->on('open', [$this, 'onOpen']);
+        $this->instance->on('message', [$this, 'onMessage']);
+        $this->instance->on('close', [$this, 'onClose']);
+
+        $this->instance->start();
+    }
+
+    public function onOpen($server, $fd)
+    {
+        print_r("客户端: {$fd} 连接到服务器");
+    }
+
+    public function onMessage($server, $frame)
+    {
+        print_r("客户端： {$frame->fd} 向服务器发送了信息\n$frame->data");
+        $server->push($frame->fd, "服务端回复了信息:$frame->data" . date('Y-m-d H:i:s'));
+    }
+
+    public function onClose($server, $fd)
+    {
+        print_r("客户端 $fd 断开了连接");
+    }
+}
+
+$server = new wbServer();
