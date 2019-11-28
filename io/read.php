c50696c91c6113f29ff4b7f64c3153eee0c3cbd0
869413421 <13528685024@163.com>
2019-11-28 22:19:26 +0800

    async

--- io/read.php
+++ io/read.php
@@ -9,10 +9,7 @@
 use Swoole\Async;
 
 //readfile会将所有内容读取到内存中，最多可以读取4M,所以不可以用来读取大文件.
-Async::readfile(__DIR__ . "/testRead.txt", 'readFile');
-
-function readFile($fileName, $fileContent)
-{
+Async::readfile(__DIR__ . "/testRead.txt", function ($fileName, $fileContent) {
     echo '文件名：' . $fileName . PHP_EOL;
     echo '文件内容：' . $fileContent . PHP_EOL;
-}
+});
