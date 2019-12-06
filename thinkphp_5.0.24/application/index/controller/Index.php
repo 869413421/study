45ca73f2cab5d4a01df85b453df64ee8fa1f47d7
Your Name <you@example.com>
2019-12-06 15:39:35 +0800

    sms

--- thinkphp_5.0.24/application/index/controller/Index.php
+++ thinkphp_5.0.24/application/index/controller/Index.php
@@ -9,6 +9,8 @@ class Index
     public function index()
     {
         $code = rand(1000, 9999);
-        return Sms::SendSms('13528685024', 'code:' . $code);
+        $data = Sms::SendSms('13528685024', 'code:' . $code);
+        var_dump($data);
+        echo json_encode($data);
     }
 }
